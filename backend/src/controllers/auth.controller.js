2343f99577eb454bb70bc45bf20d15ece5e92963
aalokshukla09 <shuklaraj0902@gmail.com>
2026-01-12 12:55:35 +0530

    login and logout endpoints

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -70,3 +70,41 @@ export const signupController = async (req, res) => {
         
     }
 }
+
+
+export const loginController = async (req, res) => {
+    const {email, password} = req.body;
+    try {
+        if(!email || !password) {
+            return res.status(400).json({message: "All fields are required"});
+        }
+
+        const user = await User.findOne({email});
+        if(!user) {
+            return res.status(400).json({message: "Invalid credentials"});
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch) {
+            return res.status(400).json({message: "Invalid credentials"});
+        }
+
+        generateToken(user._id, res);
+
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            profilePic: user.profilePic,
+        });
+    } catch (error) {
+        console.error("Error in loginController:", error);
+        return res.status(500).json({message: "Server Error"});
+    }
+}
+
+
+export const logoutController = async (req, res) => {
+    res.clearCookie("jwt");
+    res.status(200).json({message: "Logged out successfully"});
+}
